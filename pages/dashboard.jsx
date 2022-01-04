8c793b3a0234468f561226cd818b15568b317d6b
Fabian Ferno <fabianferno@gmail.com>
2022-01-04 07:23:55 +0530

    Add create projects link

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import useSWR from 'swr';
 import Highlight from '../components/Highlight';
+import Link from 'next/link';
 
 export default function SSRPage({ user }) {
   // const { data, error } = useSWR('/api/create', (...args) => fetch(...args).then(res => res.json()));
@@ -9,7 +10,11 @@ export default function SSRPage({ user }) {
   // if (error) return <div>failed to load</div>;
   // if (!data) return <div>loading...</div>;
 
-  return <div>hello !</div>;
+  return (
+    <div>
+      <Link href="/projects/create">Create Project</Link>
+    </div>
+  );
 }
 
 export const getServerSideProps = withPageAuthRequired();
